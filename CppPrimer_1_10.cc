2c6e3ece04d99a0691d68bb0817ed92aba48fbbc
Letty <lettycheungzl@gmail.com>
2017-11-07 10:32:19 +0800

    --val The sum of 10 to 0

---
+++ CppPrimer_1_10.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+    int sum = 0, val = 10;
+    while (val >= 0){
+        sum += val;
+        --val;
+    }
+    std::cout << "Sum of 10 to 0 is "
+              << sum << std::endl;
+    return 0;
+}
