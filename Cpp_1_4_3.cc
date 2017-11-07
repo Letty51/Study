87ac7949a51d407a0ae961759011c5c85e90ea7a
Letty <lettycheungzl@gmail.com>
2017-11-07 15:17:58 +0800

    EOF in Mac OS X is control + D

--- Cpp_1_4_3.cc
+++ Cpp_1_4_3.cc
@@ -2,7 +2,7 @@
 int main(){
     int sum = 0, value = 0;
     // 读取数据直到遇到文件尾，计算所有读入的值都和
-    while (std::cin >> value && value != 0){
+    while (std::cin >> value){
         sum += value; // 等价于 sum = sum + value;
     }
     std::cout << "Sum is :" << sum << std::endl;
