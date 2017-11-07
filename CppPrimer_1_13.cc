f1c757a1a8333d84a9b96a43ec09dc9c86d7d760
Letty <lettycheungzl@gmail.com>
2017-11-07 10:59:30 +0800

    for(){} The sum of 10 to 0

--- CppPrimer_1_13.cc
+++ CppPrimer_1_13.cc
@@ -1,4 +1,5 @@
 /* 1.4.1-1.9 */
+/*
 #include <iostream>
 int main(){
     int sum = 0;
@@ -6,5 +7,17 @@ int main(){
         sum += i;
     }
     std::cout << "Sum of 50 to 100 is " << sum << std::endl;
+    return 0;
+}
+*/
+
+/* 1.4.1-1.10 */
+#include <iostream>
+int main(){
+    int sum = 0;
+    for (int i = 10; i >= 0; --i){
+        sum += i;
+    }
+    std::cout << "The sum of 10 to 0 is " << sum << std::endl;
     return 0;
 }
