6ef5a4b2ac914b56ec99040f0771388bf7ebe014
Letty <lettycheungzl@gmail.com>
2017-11-07 15:10:33 +0800

    for(){} 输出给定两个整数范围内的所有整数

--- CppPrimer_1_13.cc
+++ CppPrimer_1_13.cc
@@ -12,6 +12,7 @@ int main(){
 */
 
 /* 1.4.1-1.10 */
+/*
 #include <iostream>
 int main(){
     int sum = 0;
@@ -19,5 +20,25 @@ int main(){
         sum += i;
     }
     std::cout << "The sum of 10 to 0 is " << sum << std::endl;
+    return 0;
+}
+*/
+
+/* 1.4.1-1.11 */
+#include <iostream>
+int main(){
+    int a = 0, b = 0;
+    std::cout << "Please enter two int numbers:" << std::endl;
+    std::cin >> a >> b;
+    if (a <= b){
+        for (int i = a; i <= b; ++i){
+            std::cout << i << std::endl;
+        }
+    }
+    else {
+        for (int i = b; i <= a; ++i){
+            std::cout << i << std::endl;
+        }
+    }
     return 0;
 }
